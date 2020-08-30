bbc688f962c92d6890c6d09e2333af17ec42a128
bigopon <bigopon.777@gmail.com>
2020-08-30 23:27:37 +1000

    feat(dep-collector-switcher): initial implementation

---
+++ packages/runtime/src/observation/dep-collector-switcher.ts
@@ -0,0 +1,41 @@
+import { IObserverLocator } from './observer-locator';
+import { ISubscriber } from '../observation';
+
+export interface IDepCollector extends ISubscriber {
+  observerLocator: IObserverLocator;
+}
+
+const toStringTag = Object.prototype.toString;
+let currentCollector: IDepCollector | null = null;
+const collectors: IDepCollector[] = [];
+
+export const DepCollectorSwitcher = new class {
+
+  get current(): IDepCollector | null {
+    return currentCollector;
+  }
+
+  peek(): IDepCollector | null {
+    return currentCollector;
+  }
+
+  enter(collector: IDepCollector): void {
+    if (collectors.indexOf(collector) > -1) {
+      throw new Error('Already has observer ${observer.id} in the stack');
+    }
+    if (collector === null) {
+      throw new Error('Null was given to SwitchObserver.enter');
+    }
+    collectors.push(collector);
+    currentCollector = collector;
+  }
+
+  exit(collector: IDepCollector): void {
+    if (currentCollector !== collector) {
+      throw new Error('${collector.id} is not currently collecting');
+    }
+
+    collectors.pop();
+    currentCollector = collectors.length > 0 ? collectors[collectors.length - 1] : null;
+  }
+}
