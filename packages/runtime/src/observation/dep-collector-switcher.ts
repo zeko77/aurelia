b9af2fb2e5559465ba6cea9b1872730c28da36c7
bigopon <bigopon.777@gmail.com>
2020-09-04 15:13:36 +1000

    refactor(switcher): use loose fns first

--- packages/runtime/src/observation/dep-collector-switcher.ts
+++ packages/runtime/src/observation/dep-collector-switcher.ts
@@ -1,41 +1,49 @@
 import { IObserverLocator } from './observer-locator';
-import { ISubscriber } from '../observation';
+import { ISubscriber, ICollectionSubscriber } from '../observation';
 
 export interface IDepCollector extends ISubscriber {
   observerLocator?: IObserverLocator;
 }
 
-const toStringTag = Object.prototype.toString;
-let currentCollector: IDepCollector | null = null;
+let $currentCollector: IDepCollector | null = null;
 const collectors: IDepCollector[] = [];
+const collectingStatus: boolean[] = [];
+
+export let collecting = false;
+
+// todo: layer based collection pause/resume?
+export function pauseSubscription() {
+  collecting = false;
+}
+
+export function resumeSubscription() {
+  collecting = true;
+}
+
+export function getCurrentSubscriber(): IDepCollector | null {
+  return $currentCollector;
+}
+
+export function enterSubscriber(subscriber: ISubscriber): void {
+  if ($currentCollector == null) {
+    collectors[0] = $currentCollector = subscriber;
+    return;
+  }
+  if ($currentCollector === subscriber) {
+    throw new Error('Already in this subscriber {subscriber.id}');
+  }
+  collectors.push($currentCollector = subscriber);
+}
+
+export function exitSubscriber(subscriber: ISubscriber): void {
+  if ($currentCollector == null || $currentCollector !== subscriber) {
+    throw new Error('${subscriber.id} is not currently collecting');
+  }
+
+  collectors.pop();
+  $currentCollector = collectors.length > 0 ? collectors[collectors.length - 1] : null;
+}
 
 export const DepCollectorSwitcher = new class {
 
-  get current(): IDepCollector | null {
-    return currentCollector;
-  }
-
-  peek(): IDepCollector | null {
-    return currentCollector;
-  }
-
-  enter(collector: IDepCollector): void {
-    if (collectors.indexOf(collector) > -1) {
-      throw new Error('Already has observer ${observer.id} in the stack');
-    }
-    if (collector === null) {
-      throw new Error('Null was given to SwitchObserver.enter');
-    }
-    collectors.push(collector);
-    currentCollector = collector;
-  }
-
-  exit(collector: IDepCollector): void {
-    if (currentCollector !== collector) {
-      throw new Error('${collector.id} is not currently collecting');
-    }
-
-    collectors.pop();
-    currentCollector = collectors.length > 0 ? collectors[collectors.length - 1] : null;
-  }
-}
+}();
