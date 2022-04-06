4c5ccce7ef0c5457037d2fa937e08f9ffbc83ae8
AureliaEffect <aurelia@bluespire.com>
2022-04-06 00:17:39 +0000

    chore(all): add latest build artifacts

--- packages-cjs/parcel-transformer/dist/cjs/index.js
+++ packages-cjs/parcel-transformer/dist/cjs/index.js
@@ -2,7 +2,48 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-const placeholder = null;
+var plugin = require('@parcel/plugin');
+var SourceMap = require('@parcel/source-map');
+var pluginConventions = require('@aurelia/plugin-conventions');
+var path = require('path');
 
-exports.placeholder = placeholder;
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var SourceMap__default = /*#__PURE__*/_interopDefaultLegacy(SourceMap);
+
+var index = new plugin.Transformer({
+    async loadConfig({ config }) {
+        try {
+            const conf = await config.getConfig([], { packageKey: 'aurelia' });
+            return conf ? conf.contents : {};
+        }
+        catch (e) {
+            return {};
+        }
+    },
+    async transform({ asset, config, options }) {
+        // parcel conventions puts app's index.html inside src/ folder.
+        if (asset.filePath.endsWith('src/index.html'))
+            return [asset];
+        const source = await asset.getCode();
+        const result = pluginConventions.preprocess({
+            path: path.relative(options.projectRoot, asset.filePath.slice()),
+            contents: source
+        }, config);
+        if (!result) {
+            return [asset];
+        }
+        asset.setCode(result.code);
+        const map = new SourceMap__default['default']();
+        map.addVLQMap(result.map);
+        asset.setMap(map);
+        if (asset.type === 'html') {
+            asset.type = 'js';
+        }
+        // Return the asset
+        return [asset];
+    }
+});
+
+exports['default'] = index;
 //# sourceMappingURL=index.js.map
