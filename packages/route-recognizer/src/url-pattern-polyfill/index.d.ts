c968850f356a5ef322cc1fe52fff7d481d3463ed
Sayan751 <sayan751@gmail.com>
2022-07-10 20:28:06 +0200

    chore(route-recognizer): cleanup

--- packages/route-recognizer/src/url-pattern-polyfill/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type * as Types from "./types";
-
-declare global {
-  class URLPattern extends Types.URLPattern {}
-  type URLPatternInit = Types.URLPatternInit;
-  type URLPatternResult = Types.URLPatternResult;
-  type URLPatternComponentResult = Types.URLPatternComponentResult;
-}
